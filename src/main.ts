7a928b170375e16578cf8b458592d132074908aa
JackWilhelm <jgwilhel@ucsc.edu>
2024-11-02 12:53:32 -0700

    Alert Button

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,11 @@
 // todo
+import "./style.css";
+
+const app: HTMLDivElement = document.querySelector("#app")!;
+
+const alertButton: HTMLButtonElement = document.createElement("button");
+alertButton.innerHTML = "ALERT";
+app.append(alertButton);
+alertButton.addEventListener("click", () => {
+  alert("you clicked the button!");
+});
